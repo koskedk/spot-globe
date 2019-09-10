36c2f234078be37a46e1c14baff06ff3a7cb1536
Koske Kimutai <koskedk@live.com>
2019-09-10 12:52:30 +0300

    adding ssl

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,13 +4,17 @@ import { Transport } from '@nestjs/microservices';
 import { SeederModule } from './infrastructure/seeder';
 import { log } from 'util';
 import { Logger } from '@nestjs/common';
-import { fs } from 'fast-glob/out/utils';
 import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
 import { join } from 'path';
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/demo-key.pem'),
+    cert: fs.readFileSync('./secrets/demo-cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule,{httpsOptions});
   const config = app.get(ConfigService);
   Logger.log(`starting in ${process.env.NODE_ENV} mode`);
   const microservice = app.connectMicroservice(config.QueueConfig);
